a1851dc73489711d3fa12157f47177c895f13b5d
trallagdev <trallagdev@gmail.com>
2026-01-30 13:36:51 +0100

    feat: [AP-007] add most-viewed anime query

--- src/app/services/anilist.service.ts
+++ src/app/services/anilist.service.ts
@@ -56,6 +56,19 @@ export class AnilistService {
       .pipe(map((response) => response.Page.media.map((media) => this.mapAnimeSummary(media))));
   }
 
+  getMostViewedAnime(perPage = 8): Observable<AnimeSummary[]> {
+    const variables: SearchQueryVariables = {
+      page: 1,
+      perPage,
+      sort: 'POPULARITY_DESC',
+      isAdult: false,
+    };
+
+    return this.client
+      .execute<SearchQueryResponse, SearchQueryVariables>(SEARCH_QUERY, variables)
+      .pipe(map((response) => response.Page.media.map((media) => this.mapAnimeSummary(media))));
+  }
+
   getAnimeDetailsBySlug(slug: string): Observable<AnimeDetail> {
     return this.client
       .execute<AnimeDetailQueryResponse, AnimeDetailQueryVariables>(ANIME_DETAIL_QUERY, {
